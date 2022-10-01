1173750ec4605b9ea2821782b1f5ff7541f0d5eb
tendra <akatendra@gmail.com>
2022-10-01 11:36:13 +0300

    Visualisation logging separated into separate file.

--- scraper.py
+++ scraper.py
@@ -135,6 +135,14 @@ def parse_html_kvartiry_vtorichka(html):
     item_ids_from_db = database.get_item_ids('kvartiry_vtorichka')
     logger.debug(
         f'Number of item_ids are already exist in database: {len(item_ids_from_db)}')
+    item_ids_list = []
+    for item in items:
+        item_ids_list.append(item['id'])
+    item_ids_set = set(item_ids_list)
+    logger.debug(f'item_tuple: {item_ids_set}')
+    item_ids_to_parse = item_ids_set.difference(item_ids_from_db)
+    logger.debug(f'items_to_parse: {item_ids_to_parse}')
+
     for item in items:
         # We intercept the error in case some fields are not filled while
         # parsing. An error during parsing causes the whole process to stop.
@@ -142,7 +150,7 @@ def parse_html_kvartiry_vtorichka(html):
         try:
             item_id = item['id']
             logger.debug(f'Detected item_id:  {item_id}')
-            if item_id in item_ids_from_db:
+            if item_id not in item_ids_to_parse:
                 logger.debug(
                     f'Detected item_id is already exist in database: {item_id} | Skipped...')
                 logger.debug(
